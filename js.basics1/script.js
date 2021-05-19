b36d1fa1c286862d08637c7bf689c3f7e6b1dec8
Zendeni <53412927+Zendeni@users.noreply.github.com>
2021-05-19 20:55:52 +0200

    Add files via upload

---
+++ js.basics1/script.js
@@ -0,0 +1,18 @@
+
+
+console.log("Hello Winc Academy")
+
+
+let = myName = "Erik";
+console.log(myName);
+
+
+console.log(4 + 4);
+console.log('4' + '4');
+console.log(4 - 4);
+console.log(4 * 4);
+console.log(4 / 4);
+console.log(4 % 4);
+
+let = myAge = '31';
+console.log(myAge);
