04ed8569bdd12c1ae0e87c0772cf7164083f1524
Zendeni <buskes.e@gmail.com>
2021-06-08 21:53:01 +0200

    arrow

---
+++ Basic arrow functions/main.js
@@ -0,0 +1,13 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions()
+
+const fivePlusSeven = () => 5 + 7; fivePlusSeven();
+
+// Opdracht: Schrijf een arrow function, op 1 regel (dus met een impliciete return statement), die de params a en b bij-elkaar optelt. Noem de functie myFunction.
+myFunction = () => a + b;
+
+addFive = (a) => a + 5;
+
+createObject = () => { greeting: "hoi" }
