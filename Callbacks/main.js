ea927073813ecfecec074df08481564192867d3d
Zendeni <53412927+Zendeni@users.noreply.github.com>
2021-07-11 20:05:25 +0200

    Add files via upload

--- Callbacks/main.js
+++ Callbacks/main.js
@@ -1,10 +1,12 @@
 
 const vak = "wiskunde"
 
-setTimeout(function huiswerkMaken(vak, callback) {
-    console.log("Ok, ok, ik ga nu mijn", { vak }, "huiswerk maken");
-    callback()
-}, 2000)
+function huiswerkMaken(vak, callback) {
+    setTimeout(() => {
+        console.log("Ok, ok, ik ga nu mijn", `${vak}`, "huiswerk maken");
+        callback()
+    }, 2000)
+}
 
 function klaarMetHuiswerk() {
     console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!");
